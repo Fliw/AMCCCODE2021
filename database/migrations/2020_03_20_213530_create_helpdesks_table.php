b25f7c1770a9022e35cce786b131814a7ecf14c9
Fajar Ru <kzofajar@gmail.com>
2020-03-21 17:03:24 +0700

    Add Helpdesks table migration

---
+++ database/migrations/2020_03_20_213530_create_helpdesks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHelpdesksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('helpdesks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->enum('type', ['contact', 'docs']);
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('target');
+            $table->string('workdays')->default('0');
+            $table->string('from', 5)->default('00:00');
+            $table->string('to', 5)->default('00:00');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('helpdesks');
+    }
+}
