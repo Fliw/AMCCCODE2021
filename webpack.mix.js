7af308fe63638d14423485b535720666c2f2e904
Fajar Ru <kzofajar@gmail.com>
2020-03-21 17:08:21 +0700

    Setup Stisla with Mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+require('laravel-mix-purgecss');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +12,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.sass('resources/sass/app.scss', 'public/css')
+    .js('resources/js/app.js', 'public/js').extract()
+    .purgeCss();
+mix.version();
