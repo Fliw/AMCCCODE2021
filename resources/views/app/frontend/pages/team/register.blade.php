2d3e31a6c5bef4610124810a6526bce9c3c023ec
Fajar Ru <kzofajar@gmail.com>
2020-04-19 14:44:30 +0700

    Updated login and register logo

--- resources/views/app/frontend/pages/team/register.blade.php
+++ resources/views/app/frontend/pages/team/register.blade.php
@@ -15,7 +15,7 @@
         <div class="row">
           <div class="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2">
             <div class="login-brand">
-              <img src="{{ asset('assets/img/stisla-fill.svg') }}" alt="logo" width="100" class="shadow-light rounded-circle">
+              <img src="{{ asset('code2020/img/logo-384.png') }}" alt="logo" width="192">
             </div>
             @if(session()->has('info'))
               <div class="alert alert-primary">
